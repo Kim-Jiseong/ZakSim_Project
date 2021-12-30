fc05948a7372ae05c5534fd5bda372337e04fc30
Kim-Jiseong <jisungwise@naver.com>
2021-12-31 01:27:08 +0900

    구조세팅

---
+++ zaksim/app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+# from __future__ import unicode_literals
+from django.utils import timezone
+
+# Create your models here.
+# class Post(models.Model):
+#     author = models.ForeignKey('auth.User')
+#     category = models.CharField(max_length=60)
+#     title = models.CharField(max_length=200)
+#     content = models.TextField()
+#     createdAt = models.DateTimeField(default=timezone.now)
+#     finishedAt = models.DateTimeField()
+#     success = models.BooleanField(default=False)
